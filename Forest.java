1efd33267b7a5592503a4edc60d3c44b4cc56471
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-24 15:19:22 +0530

    Delete Forest.java

--- Forest.java
+++
@@ -1,8 +0,0 @@
-public class Forest{
-	public static void birds(String[] fav){
-	int sizeOfMainParam=fav.length;
-	System.out.println("sizeOfMainParam " +sizeOfMainParam);
-	System.out.println("Element @ index 0 "+animals[0]);
-	System.out.println("Element @ index 1 "+animals[1]);
-	}
-}
