6ee31c06fb2d265cc7a51ab234bf08f7570c1e0d
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-24 15:21:06 +0530

    Add files via upload

---
+++ example1/Forest.java
@@ -0,0 +1,8 @@
+public class Forest{
+	public static void birds(String[] fav){
+	int sizeOfMainParam=fav.length;
+	System.out.println("sizeOfMainParam " +sizeOfMainParam);
+	System.out.println("Element @ index 0 "+fav[0]);
+	System.out.println("Element @ index 1 "+fav[1]);
+	}
+}
