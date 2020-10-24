3f2804b6d7865f44ffbc3ab8affb368a97b8c009
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-24 15:20:06 +0530

    Delete Toy.java

--- Toy.java
+++
@@ -1,9 +0,0 @@
-public class Toy{
-public static void main(String[] game){
-	System.out.println("JVM invoked main");
-	Pubg.supply();
-	Pubg.enemiesAhead();
-	System.out.println("exit main");
-
-}
-}
