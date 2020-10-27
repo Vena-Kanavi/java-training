02396ee0490b3c05af3740d16218162c718188d2
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-27 14:05:40 +0530

    return type age example commit

---
+++ age_example/AgePredictor.java
@@ -0,0 +1,8 @@
+public class AgePredictor{
+	public static void main(String[] person){
+		System.out.println("JVM invoked main");
+		String ageValue=Age.AgeDefine(18);
+		System.out.println("The person with this age is  "+ageValue);
+
+	}
+}
