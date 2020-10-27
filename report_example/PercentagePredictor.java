d2eb8c303a52ae27e373c85ed216f37005bda1c6
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-10-27 14:06:40 +0530

    percentage with performance commit

---
+++ report_example/PercentagePredictor.java
@@ -0,0 +1,9 @@
+public class PercentagePredictor{
+	public static void main(String[] marks){
+		System.out.println("JVM invoked main");
+		String score=Result.report(50);
+		System.out.println("Performance based on percentage is  "+score);
+        System.out.println("EXIT:: main");
+
+	}
+}
