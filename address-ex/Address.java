9e2ffeca10b077454939174af6dba00bc22d9db8
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-11-05 17:21:52 +0530

    address example commit

---
+++ address-ex/Address.java
@@ -0,0 +1,6 @@
+public class Address{
+	public static short doorNo=347;
+	public static String city="Gadag";
+	public static int pinCode=582116;
+	
+}
---
+++ address-ex/AddressTester.java
@@ -0,0 +1,9 @@
+public class AddressTester{
+	public static void main(String[] args){
+		System.out.println("use of fields");
+		short door=Address.doorNo;
+		System.out.println("door no is   "+door);
+		System.out.println("pin code is   "+Address.pinCode);
+		System.out.println("city is   "+Address.city);
+	}
+}
