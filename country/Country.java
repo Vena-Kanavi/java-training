f9e236fbf70b2fbe0e366fded38e8039f8daf77c
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-11-05 17:19:30 +0530

    country exampe commit

---
+++ country/Country.java
@@ -0,0 +1,7 @@
+public class Country{
+	public static short noOfStates=29;
+	public static byte unionTerrotories=7;
+	public static String countryName="India";
+	public static int areaOfCountry=3287263;
+	public static String stateName="Karnataka";
+}
---
+++ country/CountryTester.java
@@ -0,0 +1,12 @@
+public class CountryTester{
+	public static void main(String[] args){
+		System.out.println("use of fields");
+		System.out.println("country name   "+Country.countryName);
+		int door=Country.areaOfCountry;
+		System.out.println("areaOfCountry is   "+door);
+		System.out.println("no of states   "+Country.noOfStates);
+		System.out.println("stateName is   "+Country.stateName);
+		System.out.println("unionTerrotories is   "+Country.unionTerrotories);
+
+	}
+}
