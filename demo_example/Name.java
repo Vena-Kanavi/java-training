619103ce86ffc6ee2e004fe37e016900f181d7e2
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-11-10 18:28:17 +0530

    example done in eclipse

---
+++ demo_example/Name.java
@@ -0,0 +1,13 @@
+package com.xworkz.demo_example;
+
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int val=0;val<args.length;val++) {
+		String name=args[val];
+        System.out.println("Name is  "+name);
+		}
+	}
+
+}
