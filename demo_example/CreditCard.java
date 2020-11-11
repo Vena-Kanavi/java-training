bfcb9212dc02b80ce4658236043c2350e8fd4aac
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-11-11 20:52:24 +0530

    credit card example

---
+++ demo_example/CreditCard.java
@@ -0,0 +1,10 @@
+package com.xworkz.demo_example;
+
+public class CreditCard {
+
+	public static void main(String[] args) {
+		System.out.println("main method invoked");
+		CreditCardEstimater.expenditure(25);
+	}
+
+}
