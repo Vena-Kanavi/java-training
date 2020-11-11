780c68ccfd2e4932ae60c261c1c57bf679cdc018
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2020-11-11 20:53:34 +0530

    pocket money example

---
+++ demo_example/Pocket.java
@@ -0,0 +1,12 @@
+package com.xworkz.demo_example;
+
+public class Pocket {
+
+	public static void main(String[] args) {
+		PocketMoneyEstimator.request(25, "monday");
+		PocketMoneyEstimator.expenditure(5);
+		
+
+	}
+
+}
