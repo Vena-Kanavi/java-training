092eb55b03e91ffad5661c60f7e2f300b8233179
Vena Kanavi <72905178+Vena-Kanavi@users.noreply.github.com>
2021-02-10 21:59:51 +0530

    restaurant application

---
+++ moon-star/java/com/xworkz/moon/dto/MenuDTO.java
@@ -0,0 +1,42 @@
+package com.xworkz.moon.dto;
+
+public class MenuDTO {
+	
+	private String itemName;
+	private double price;
+	private Type type;
+	
+	
+	public enum Type{
+		VEG,NONVEG
+	}
+
+
+	public MenuDTO(String itemName, double price, Type type) {
+		super();
+		this.itemName = itemName;
+		this.price = price;
+		this.type = type;
+	}
+
+
+	public String getItemName() {
+		return itemName;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public Type getType() {
+		return type;
+	}
+
+
+	@Override
+	public String toString() {
+		return "MenuDTO [itemName=" + itemName + ", price=" + price + ", type=" + type + "]";
+	}
+}
